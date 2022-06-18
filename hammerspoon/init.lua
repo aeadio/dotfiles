bf2f53b20be07c2e8036be9fe4b75e32e6651ebe
Anthony Eadicicco <dotfiles@aead.xyz>
2022-06-18 00:41:19 -0400

    Hammerspoon: Load config files lexographically

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,6 @@
 -- Load all config
+local conf_items = {}
+
 for item in hs.fs.dir(hs.configdir) do
   if item == 'init.lua' then goto continue end
   if not item:find('%.lua$') then goto continue end
@@ -13,7 +15,17 @@ for item in hs.fs.dir(hs.configdir) do
     goto continue
   end
   
-  print(('config: loading %q'):format(item))
+  print(('config: adding %q'):format(item))
+  table.insert(conf_items, item)
+  
+  ::continue::
+end
+
+table.sort(conf_items)
+
+for _, item in ipairs(conf_items) do
+  local path = ('%s/%s'):format(hs.configdir, item)
+  
   local f, err = loadfile(path)
   if not f then
     print(('config: %q failed to parse: %s'):format(item, err))
